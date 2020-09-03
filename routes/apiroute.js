a9f01f08141d9f712b926a61620a45be987119f4
Chris Collins <ccollins36@icloud.com>
2020-09-02 22:29:46 -0400

    comp. get and post api routs

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -1,21 +1,40 @@
 const router = require("express").Router();
 const db = require("../db/db.json");
 const shortid = require('shortid');
-router.get("/notes", function(req,res){
+const fs = require("fs");
+const path = require("path");
+router.get("/notes", function (req, res) {
     res.json(db)
 });
-router.post("/notes", function(req,res){
+router.post("/notes", function (req, res) {
     // res.json(db)
     var newNote = {
         title: req.body.title,
         text: req.body.text,
-        id:shortid.generate()
+        id: shortid.generate()
     }
+    db.push(newNote);
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(db));
+    return res.json(db)
 });
-router.delete("/notes/:id", function(req,res){
-    res.json(db)
+router.delete("/notes/:id", function (req, res) {
+    // res.json(db)
+    // fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (error, data) {
+    //     console.log (data)
+    //     let results = data;
+    //     if (error) {
+    //         console.log(error)
+    //     } else {
+            let out = db.filter((note) => {
+                note.id !== req.params.id
+            });
+            fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(out));
+            console.log (out)
+            return res.json(out);
+
+        // }
+    // });
 });
 
 
 module.exports = router;
- 
